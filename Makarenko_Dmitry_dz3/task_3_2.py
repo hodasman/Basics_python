647beefc4b54e974262d4449f1919a5bcafa0f46
Dmitry <hodas.work@gmail.com>
2022-04-11 18:42:19 +0300

    add task_3_1 and task_3_2

---
+++ Makarenko_Dmitry_dz3/task_3_2.py
@@ -0,0 +1,12 @@
+def num_translate_adv(num: str) -> str:
+    num_dict = {'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять', 'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
+    if num.istitle():
+        str_out = num_dict.get(num.lower())
+        return str_out.capitalize()
+    else:
+        str_out = num_dict.get(num)
+        return str_out
+
+
+
+print(num_translate_adv('Ten'))
