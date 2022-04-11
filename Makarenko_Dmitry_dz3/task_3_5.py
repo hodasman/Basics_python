84d4df53c3d61ec0dfb7da26894bac3d47262079
Dmitry <hodas.work@gmail.com>
2022-04-11 19:52:05 +0300

    task_3_5 b- version with flag - repeat yes or no

--- Makarenko_Dmitry_dz3/task_3_5.py
+++ Makarenko_Dmitry_dz3/task_3_5.py
@@ -1,15 +1,28 @@
-def get_jokes(n: int) -> list:
-    """возвращает n шуток, сформированных из трех случайных слов, взятых из трёх списков (по одному из каждого)"""
-    from random import choice
+def get_jokes(n: int, repeat='yes') -> list:
+    """возвращает n шуток, сформированных из трех случайных слов, взятых из трёх списков (по одному из каждого), флаг repeat позволяет или запрещает повторы слов в шутках"""
+    from random import choice, randrange
     list_out = []
     nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
     adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
     adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
     i = 0
-    while i < n:
-        list_out.append(f'{choice(nouns)} {choice(adverbs)} {choice(adjectives)}')
-        i += 1
-    return list_out
+    if repeat == 'yes':
+        while i < n:
+            list_out.append(f'{choice(nouns)} {choice(adverbs)} {choice(adjectives)}')
+            i += 1
+        return list_out
+    else:
+        if n > len(nouns):
+            print('Количество слов в списке меньше, чем введенное вами количество неповторяющихся шуток')
+        else:
+            while i < n:
+                a = randrange(0, len(nouns))
+                b = randrange(0, len(adverbs))
+                c = randrange(0, len(adjectives))
+                list_out.append(f'{nouns[a]} {adverbs[b]} {adjectives[c]}')
+                del nouns[a], adverbs[b], adjectives[c]
+                i += 1
+            return list_out
 
 
-print(get_jokes(5))
+print(get_jokes(5, 'no'))
