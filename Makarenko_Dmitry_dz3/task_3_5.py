01a5f99241070e26ec583bbb8365ffa8007bec67
Dmitry <hodas.work@gmail.com>
2022-04-11 19:22:35 +0300

    add task_3_5 a-version

---
+++ Makarenko_Dmitry_dz3/task_3_5.py
@@ -0,0 +1,14 @@
+def get_jokes(n: int) -> list:
+    from random import choice
+    list_out = []
+    nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
+    adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
+    adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
+    i = 0
+    while i < n:
+        list_out.append(f'{choice(nouns)} {choice(adverbs)} {choice(adjectives)}')
+        i += 1
+    return list_out
+
+
+print(get_jokes(5))
