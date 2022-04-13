36177d15589f8ba23acfdec766dc69ca1eee8792
Dmitry <hodas.work@gmail.com>
2022-04-13 20:02:15 +0300

    write documentation in task_3_1

--- Makarenko_Dmitry_dz3/task_3_1.py
+++ Makarenko_Dmitry_dz3/task_3_1.py
@@ -1,4 +1,5 @@
 def num_translate(num: str) -> str:
+    """переводит числительные от 0 до 10 c английского на русский язык"""
     num_dict = {'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять', 'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
     str_out = num_dict.get(num)
     return str_out
--- Makarenko_Dmitry_dz3/task_3_2.py
+++ Makarenko_Dmitry_dz3/task_3_2.py
@@ -1,4 +1,5 @@
 def num_translate_adv(num: str) -> str:
+    """переводит числительные от 0 до 10 c английского на русский язык"""
     num_dict = {'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять', 'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
     if num.istitle():
         str_out = num_dict.get(num.lower())
