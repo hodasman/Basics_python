a301b76b9f61d2d2ddd3d0d412ca647fbc50499c
Dmitry <hodas.work@gmail.com>
2022-04-21 21:37:03 +0300

    edit task_5_3

--- Makarenko_Dzmitry_dz_5/task_5_3.py
+++ Makarenko_Dzmitry_dz_5/task_5_3.py
@@ -1,16 +1,17 @@
-tutors = [
-    'Иван', 'Анастасия', 'Петр', 'Сергей',
-    'Дмитрий', 'Борис', 'Елена'
-]
-klasses = [
-    '9А', '7В', '9Б', '9В', '8Б', '10А'
-]
+tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена']
+klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А']
 
-def generator():
+
+def check_gen(tutors: list, klasses: list):
     for i in range(len(tutors)):
         if i >= len(klasses):
             yield (tutors[i], None)
         else:
             yield (tutors[i], klasses[i])
-print(type(generator()))
-print(*generator(), sep='')
+
+
+generator = check_gen(tutors, klasses)
+print(type(generator)) # добавьте здесь доказательство, что создали именно генератор
+for _ in range(len(tutors)):
+    print(next(generator))
+# next(generator)  # если раскомментировать, то должно падать в traceback по StopIteration
