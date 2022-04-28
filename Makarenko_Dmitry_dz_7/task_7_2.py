bebb4738b4d7ba594309f01849b651fabc4e326d
Dmitry <hodas.work@gmail.com>
2022-04-28 11:04:54 +0300

    add task_7_1 and task_7_2

---
+++ Makarenko_Dmitry_dz_7/task_7_2.py
@@ -0,0 +1,29 @@
+import os
+
+with open('config.yaml', 'r') as f:
+    for line in f:
+        tuple = line.split()
+        path = tuple[0]
+        if not os.path.exists(path):
+            os.mkdir(path)
+        dirs = tuple[1].strip('[]').split(',')
+        for dir in dirs:
+            p = os.path.join(path, dir)
+            if not os.path.exists(p):
+                os.mkdir(p)
+        files = tuple[2].strip('[]').split(',')
+        for file in files:
+            if not file:
+                break
+            with open(os.path.join(path, file), 'w+', encoding='utf-8') as g:
+                g.write('')
+
+
+
+
+
+
+
+
+
+
