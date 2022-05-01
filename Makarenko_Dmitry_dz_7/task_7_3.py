ba11af313016a816ea717b48b4461d5506dc24eb
Dmitry <hodas.work@gmail.com>
2022-05-01 12:23:30 +0300

    add task_7_4

--- Makarenko_Dmitry_dz_7/task_7_3.py
+++ Makarenko_Dmitry_dz_7/task_7_3.py
@@ -4,12 +4,15 @@ from shutil import copy
 
 root_dir = 'my_project' # Задаем корневую папку
 main_templates = 'my_project/mainapp/templates' # Задаем путь куда будем копировать все найденные шаблоны
-for root, dirs, files in os.walk(root_dir):
-    for dir in dirs:
-        if dir == 'templates':
-            f = os.path.join(root, 'templates')
-            if f != main_templates:
-                shutil.copytree(f, main_templates, dirs_exist_ok=True)
+try:
+    for root, dirs, files in os.walk(root_dir):
+        for dir in dirs:
+            if dir == 'templates':
+                f = os.path.join(root, 'templates')
+                if f != main_templates:
+                    shutil.copytree(f, main_templates, dirs_exist_ok=True)
+except (FileNotFoundError, EOFError) as e:
+    print(f'Error directory: {e}')
 
 
 
