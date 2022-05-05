b3565cb7c05aaca96acd2a623422f667af7e1522
Dmitry <hodas.work@gmail.com>
2022-05-05 20:45:13 +0300

    add task_8_3

---
+++ Makarenko_Dmitry_dz_8/task_8_3.py
@@ -0,0 +1,18 @@
+def type_logger(func):
+   def wrapper (*args, **kwargs):
+      list = []
+      for arg in args:
+         list.append(f'{func.__name__}({arg}: {type(arg)}) = {func(arg)}: {type(func(arg))}')
+      for val in kwargs.values():
+         list.append(f'{func.__name__}({val}: {type(val)}) = {func(val)}: {type(func(val))}')
+      print(', '.join(list))
+   return wrapper
+
+
+
+
+@type_logger
+def calc_cube(x):
+   return x ** 3
+
+a = calc_cube(5, 7, 10, num=6, int=12)
